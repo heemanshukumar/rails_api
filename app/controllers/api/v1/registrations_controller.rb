db4d89d7515d31e892fc800a4c8dd10d8bbca4d7
Himanshu Kumar <heemanshudweevedee@gmail.com>
2021-03-10 19:56:21 +0530

    Update registrations_controller.rb

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,35 +2,35 @@ class Api::V1::RegistrationsController < ActionController::Base
 
   def create
   	begin
-    	ActiveRecord::Base.transaction do
-        if params[:user][:password] == 	params[:user][:password_confirmation]
-  	    	@user = User.new(user_params)
-  	    	if @user.save
-            @token = @user.api_tokens.create(token: @user.generate_token)
-  	    		render json: {
-  	    			success: true,
-    					message: "User registered successfully!",
-              token: @token.token
-  	    		}
-  	    	else
-  	    		render json:{
-  	    			success: false,
-  		      	message: @user.errors.full_messages.join(", ")
-  	    		}
-  	    	end
-        else
-          render json:{
-            success: false,
-            message: "Password does not match"
-          }
-        end
-	    end
-    rescue Exception => err
-    	render json:{
+    		ActiveRecord::Base.transaction do
+			if params[:user][:password] == 	params[:user][:password_confirmation]
+				@user = User.new(user_params)
+				if @user.save
+					@token = @user.api_tokens.create(token: @user.generate_token)
+					render json: {
+						success: true,
+						message: "User registered successfully!",
+						token: @token.token
+					}
+				else
+					render json:{
+						success: false,
+						message: @user.errors.full_messages.join(", ")
+					}
+				end
+			else
+			  render json:{
+			    success: false,
+			    message: "Password does not match"
+			  }
+			end
+		    end
+    	rescue Exception => err
+    		render json:{
   			success: false,
-      	message: err.message
+      			message: err.message
   		}
-    end
+    	end
   end
 
   
