f54cffaff7afe4dcadbcc793bf6c239574f9b10f
Himanshu Kumar <heemanshudweevedee@gmail.com>
2021-03-10 20:14:16 +0530

    Update user_controller.rb

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,40 +1,47 @@
-class Api::V1::UsersController < Api::V1::ApplicationController
+class Api::V1::UsersController < Api::V1::ApplicationController 
+	skip_before_action :check_terms
 
-	skip_before_action :authenticate, only: [:validate_otp], raise: false
+	def update_terms
+		if params[:user][:terms_accepted].present? 
 
-	def me
-		render json: { id: current_user.id, email: current_user.email, username: current_user.username, two_factor_enabled: current_user.two_factor_enabled, contact: current_user.contact }
-    	end
-    
-	def enable_otp_validate_auth
-		if current_user.current_otp == params[:user][:code]
-			enable_otp
+			if @user.update(terms_accepted: true)
+				render json:{
+					success: true
+				}
+			else
+				render json:{
+					success: false,
+					message: @user.errors.full_messages.join(', ')
+				}
+			end
 		else
-			render json: {success: false, message: "Please Enter a valid code"}
+			render json:{
+				success: false,
+				message: "Please accept privacy and eligibility."
+			}
 		end
 	end
 
-	def disable_otp_validate_auth
-		if two_factor_auth_verified?(params[:user][:code])
-			disable_otp
-		else
-			render json: {success: false, message: "Please Enter a valid code"}
-		end
+	def user_details
+		render json: {
+			success: true,
+			user: @user
+		}
 	end
-# 	Check otp validation for login
-	def validate_otp
-		user = User.where(["lower(username) = :value OR lower(email) = :value", {value: params[:user][:login].strip.downcase}]).first
-    		if(user.current_otp == params[:user][:code])
-            		sign_in(:user, user)
-            		jwt = Auth.issue({user: user.id})
-            
-            		render json: {  success: true,
-                            access_token: jwt,
-                            user: user.as_json(only: [:id, :email, :username, :two_factor_enabled, :portfolio_type,:contact]),
-                            location: Geocoder.search(ip).first.try(:country)
-                        }
-        	else
-        		render json: {success: false }
-        	end
-	end
+
+	def recheck_terms
+	    if @user.terms_accepted
+	      render json:{
+		success: true}
+	    else
+	      render json:{
+		success: false,
+		message: 'Please accept privacy and eligibility'
+	      }
+	    end
+  	end
+
+
+
+
 end
